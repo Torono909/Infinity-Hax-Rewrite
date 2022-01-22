782f6b596f43cdec2d1581ce085047dc2d42b547
FunWithAlbiBussiness <98217558+FunWithAlbiBussiness@users.noreply.github.com>
2022-01-22 14:41:42 +0100

    Fixed bugs and updated stuff

--- src/main/java/me/funwithalbi/infinityhax/Main.java
+++ src/main/java/me/funwithalbi/infinityhax/Main.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
 public class Main
 {
-    public static final String MODID = "infhax";
+    public static final String MODID = "infhx";
     public static final String NAME = "Infinity-Hax";
     public static final String VERSION = "0.12-2";
 
@@ -25,9 +25,10 @@ public class Main
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // actual activated sender
-        logger.info("Loaded Infinity Hax Infinity incursion on top!")
-        // some obsidian logger
+		// verify loading
+        logger.info("Loaded Infinity Hax. Infinity incursion remastered on top!")
+		
+        // some obsidian logger test code
         logger.info("OBBY >> {}", Blocks.OBSIDIAN.getRegistryName());
     }
 }
