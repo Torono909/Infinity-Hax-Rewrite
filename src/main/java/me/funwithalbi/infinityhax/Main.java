752f6320c7898ef01cb7774cd88868b6df031883
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-01-22 19:02:41 +0530

    Update Main.java

--- src/main/java/me/funwithalbi/infinityhax/Main.java
+++ src/main/java/me/funwithalbi/infinityhax/Main.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
 public class Main
 {
-    public static final String MODID = "infhx";
+    public static final String MODID = "infhax";
     public static final String NAME = "Infinity-Hax";
     public static final String VERSION = "0.12-2";
 
@@ -25,7 +25,9 @@ public class Main
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some obsidian log4j logger
+        // actual activated sender
+        logger.info("Loaded Infinity Hax Infinity incursion on top!")
+        // some obsidian logger
         logger.info("OBBY >> {}", Blocks.OBSIDIAN.getRegistryName());
     }
 }
