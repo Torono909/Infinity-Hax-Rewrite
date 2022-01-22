b238c36da326df01a5b0bcb7f20fc6321e7a4c39
FunWithAlbiBussiness <98217558+FunWithAlbiBussiness@users.noreply.github.com>
2022-01-22 14:22:16 +0100

    Fixed bugs in the config

---
+++ src/main/java/me/funwithalbi/infinityhax/Main.java
@@ -0,0 +1,31 @@
+package me.funwithalbi.infinity-hax;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "infhx";
+    public static final String NAME = "Infinity-Hax";
+    public static final String VERSION = "0.12-2";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some obsidian log4j logger
+        logger.info("OBBY >> {}", Blocks.OBSIDIAN.getRegistryName());
+    }
+}
