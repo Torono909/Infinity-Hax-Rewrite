8a5bb3f82214b133d52e225ca0d4cd477557efa5
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-02 00:07:28 +0530

    Update ModuleManager.java

--- src/main/java/me/funwithalbi/infinityhax/module/ModuleManager.java
+++ src/main/java/me/funwithalbi/infinityhax/module/ModuleManager.java
@@ -1,5 +1,46 @@
 package me.funwithalbi.infinityhax.module;
 
-public class ModuleManager {
+import java.util.ArrayList;
 
+public class ModuleManager {
+  
+  private static ArrayList<Module> modules;
+  
+  public ModuleManager() {
+    mods = new ArrayList<Module>();
+    //COMBAT
+    
+    //EXPLOIT
+    
+    //PLAYER
+    
+    //MOVEMENT
+    
+    //RENDER
+    
+    //MISC
+    
+  }
+  public static void newModule(Module m) {
+    modules.add(m);
+  }
+  public static ArrayList<Module> getModules() {
+    return modules;
+  }
+  public static void onUpdate() {
+    for(Module m : modules) {
+      m.onUpdate();
+    }
+  }
+  public static void onRender() {
+    for(Module m : modules) {
+      m.onRender();
+    }
+  }
+  public static void onKey(int k) {
+    for(Module m : modules) {
+      if (m.getKey == k)
+      m.toggle();
+    }
+  }
 }
