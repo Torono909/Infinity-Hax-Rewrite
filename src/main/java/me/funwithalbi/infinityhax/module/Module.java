7318b03795687765a198cd6076292ce7953282fc
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-01 22:31:27 +0530

    Update Module.java

--- src/main/java/me/funwithalbi/infinityhax/module/Module.java
+++ src/main/java/me/funwithalbi/infinityhax/module/Module.java
@@ -1,7 +1,45 @@
 package me.funwithalbi.infinityhax.module;
 
+import net.minecraft.client.Minecraft;
+
 public class Module {
-
+  /* Torono909/Google_was_my_idea is a legend */
+  
   protected Minecraft mc = minecraft.getMinecraft();
-
+  private String name;
+  private int key;
+  private boolean toggled;
+  private Category category;
+  
+  public Module(String nm, int k, Category c) {
+    name = nm;
+    key = k;
+    category = c;
+    toggled = false;
+  }
+  
+  public getMc() {
+    return mc;
+  }
+  public void setMc(Minecraft mc){
+    this.mc = mc;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+  public int getKey() {
+  return key;
+  }
+  public void setKey(int key) {
+    this.key = key;
+  }
+  public Category category() {
+  return category;
+  }
+  public void category(){
+    this.category = category;
+  }
 }
