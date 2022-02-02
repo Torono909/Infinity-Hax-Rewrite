30e0ca7f4ee52c2bea45706b1366e2e26ac8435b
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-02 23:59:39 +0530

    Update Infinityhax.java

--- src/main/java/me/funwithalbi/infinityhax/Infinityhax.java
+++ src/main/java/me/funwithalbi/infinityhax/Infinityhax.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = INFINITYHAX.MODID, name = INFINITYHAX.NAME, version = INFINITYHAX.VERSION)
-public class Main{
+public class Infinityhax{
     public static final String MODID = "infhx";
     public static final String NAME = "Infinity-Hax";
     public static final String VERSION = "0.12-2";
