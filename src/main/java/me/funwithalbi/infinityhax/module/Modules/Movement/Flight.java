ee0746ac02f1b25514919ac9cdbeea79b05ff853
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-03 12:03:08 +0530

    Create Flight.java

---
+++ src/main/java/me/funwithalbi/infinityhax/module/Modules/Movement/Flight.java
@@ -0,0 +1,8 @@
+package me.funwithalbi.infinityhax.module.Modules.Movement;
+
+import org.lwjgl.input.Keyboard;
+import me.funwithalbi.infinityhax.module.Module;
+
+public class Flight extends Module{
+  
+}
