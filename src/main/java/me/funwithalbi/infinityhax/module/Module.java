e1ce1c3c1f7608151f3997d0e880c97ae511cd55
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-26 20:57:03 +0530

    Update Module.java

--- src/main/java/me/funwithalbi/infinityhax/module/Module.java
+++ src/main/java/me/funwithalbi/infinityhax/module/Module.java
@@ -1,57 +1,101 @@
 package me.funwithalbi.infinityhax.module;
 
+import me.funwithalbi.infinityhax.Infinityhax;
+import me.funwithalbi.infinityhax.Category;
+import me.funwithalbi.infinityhax.setting.Setting;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 
-public class Module{
-  /* Torono909/Google_was_my_idea is a legend */
-  
-  protected Minecraft mc = minecraft.getMinecraft();
-  private String name;
-  private int key;
-  private boolean toggled;
-  private Category category;
-  
-  public Module(String nm, int k, Category c) {
-    name = nm;
-    key = k;
-    category = c;
-    toggled = false;
-  }
-  public void toggle() {
-   toggled = !toggled;
-    if (toggled) {
-      onEnable();
-    }else {
-      onDisable();
+public class Module {
+    private String name;
+    private String description;
+    private Category category;
+    private int bind;
+    private boolean enabled;
+    public final Minecraft mc = Minecraft.func_71410_x();
+
+    public Module(String name, Category category) {
+        this.name = name;
+        this.category = category;
     }
-  }
-  public void onEnable() {}
-  public void onDisable(){}
-  public void onUpdate() {}
-  public void onRender() {}
-  
-  public getMc() {
-    return mc;
-  }
-  public void setMc(Minecraft mc) {
-    this.mc = mc;
-  }
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public int getKey() {
-  return key;
-  }
-  public void setKey(int key) {
-    this.key = key;
-  }
-  public boolean isToggled() {
-    return toggled;
-  }
-  public void setToggled() {
-    this.toggled = togggled;
-  }
+
+    public Module(String name, String description, Category category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+
+    public void enable() {
+        this.setEnabled(true);
+        this.onEnable();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void disable() {
+        this.setEnabled(false);
+        this.onDisable();
+        MinecraftForge.EVENT_BUS.unregister(this);
+    }
+
+    public void toggle() {
+        if (this.isEnabled()) {
+            this.disable();
+        } else {
+            this.enable();
+        }
+    }
+
+    public boolean nullCheck() {
+        return this.mc.field_71439_g == null || this.mc.field_71441_e == null;
+    }
+
+    public void addSetting(Setting setting) {
+        Razmorozka.settingManager.addSetting(setting);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getBind() {
+        return this.bind;
+    }
+
+    public void setBind(int bind) {
+        this.bind = bind;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
