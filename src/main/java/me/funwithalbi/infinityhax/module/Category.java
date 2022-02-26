b879099ce535fc19279087f0eaf3004847cb2e00
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-02-26 20:49:40 +0530

    Update Category.java

--- src/main/java/me/funwithalbi/infinityhax/module/Category.java
+++ src/main/java/me/funwithalbi/infinityhax/module/Category.java
@@ -1,5 +1,23 @@
-package me.funwithalbi.infinityhax.module;
+package me.amfero.razmorozka.module;
 
-public enum Category{
-  COMBAT, EXPLOIT, PLAYER, MOVEMENT, RENDER, MISC
+public enum Category {
+    COMBAT("Combat"),
+    EXPLOIT("Exploit"),
+    RENDER("Visuals"),
+    MOVEMENT("Movement"),
+    MISC("Miscellaneous");
+
+    private String name;
+
+    private Category(String name) {
+        this.setName(name);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
