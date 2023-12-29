c8326dc9be11a54c6a7d0eae7aad7dc2d6a74d56
Zone <86355565+Torono909@users.noreply.github.com>
2023-12-29 19:46:30 +0530

    Create PlayerUpdateMoveStatePostEvent.java

---
+++ src/main/java/me/funwithalbi/infinityhax/events/player/PlayerUpdateMoveStatePostEvent.java
@@ -0,0 +1,7 @@
+package me.funwithalbi.infinityhax.events.player;
+
+import me.funwithalbi.infinityhax.events.bus.Cancellable;
+
+public class PlayerUpdateMoveStatePostEvent extends Cancellable {
+
+}
