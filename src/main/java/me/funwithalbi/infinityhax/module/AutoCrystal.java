48105cec6274cd3faf16e0de345da266f2d66881
Zone <86355565+Torono909@users.noreply.github.com>
2023-12-30 16:55:15 +0530

    Update AutoCrystal.java

--- src/main/java/me/funwithalbi/infinityhax/module/AutoCrystal.java
+++ src/main/java/me/funwithalbi/infinityhax/module/AutoCrystal.java
@@ -1,60 +1,128 @@
 package me.funwithalbi.infinityhax.module;
 
 import me.funwithalbi.infinityhax.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderCrystal;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class AutoCrystal extends Module {
-    private boolean shouldSwitch = true;
-    private int crystalSlot = -1;
+public class AutoCrystalModule extends Module {
+    private int placeRange = 5;
+    private int delayBetweenPlacements = 0;
+    private boolean onlyVisible = true;
+    private boolean smartDelay = true;
+    private boolean switchEnabled = true;
+    private boolean killerEnabled = false;
 
-    public AutoCrystal() {
+    private int ticksToPlace = 0;
+    private int ticksSinceLastPlace = 0;
+
+    public AutoCrystalModule() {
         super("AutoCrystal", Category.COMBAT);
         setDescription("Automatically places and detonates crystals");
-        addSetting(new Setting("Crystal Range", this, 4, 1, 6)); // Setting for crystal placement range
-        addSetting(new Setting("Silent Switch", this, false)); // Setting for silent switch
+        addSettings();
+        initialize();
+    }
+
+    private void addSettings() {
+        addSetting(new Setting("Place Range", this, placeRange, 1, 15));
+        addSetting(new Setting("Delay Between Placements", this, delayBetweenPlacements, 0, 5));
+        addSetting(new Setting("Only Visible", this, onlyVisible));
+        addSetting(new Setting("Smart Delay", this, smartDelay));
+        addSetting(new Setting("Switch Enabled", this, switchEnabled));
+        addSetting(new Setting("Killer", this, killerEnabled));
+    }
+
+    private void initialize() {
+        registerEvents();
     }
 
     @SubscribeEvent
     public void onWorldRender(RenderWorldLastEvent event) {
-        if (isEnabled() && mc.player != null) {
-            if (getSetting("Silent Switch").getBooleanValue()) {
-                if (shouldSwitch) {
-                    crystalSlot = findCrystalSlot();
-                    shouldSwitch = false;
-                }
+        if (!isEnabled() || mc.player == null)
+            return;
 
-                if (crystalSlot != -1) {
-                    mc.player.inventory.currentItem = crystalSlot;
-                    mc.playerController.updateController();
+        if (ticksToPlace > 0) {
+            ticksToPlace--;
+            return;
+        }
+
+        EntityEnderCrystal crystal = findCrystal();
+        if (crystal != null) {
+            if (killerEnabled) {
+                placeCrystal(crystal);
+            } else {
+                if (switchEnabled) {
+                    predictPlayerMovement(crystal);
+                } else {
+                    placeCrystal(crystal);
                 }
             }
-
-            for (EntityEnderCrystal crystal : mc.world.loadedEntityList.stream()
-                    .filter(entity -> entity instanceof EntityEnderCrystal)
-                    .map(entity -> (EntityEnderCrystal) entity)
-                    .toArray(EntityEnderCrystal[]::new)) {
-                if (mc.player.getDistance(crystal) <= getSetting("Crystal Range").getIntegerValue() && mc.player.canEntityBeSeen(crystal)) {
-                    mc.playerController.attackEntity(mc.player, crystal);
-                    mc.player.swingArm(EnumHand.MAIN_HAND);
-                }
-            }
+            
+            if (smartDelay)
+                ticksToPlace = calculateSmartDelay();
+            else
+                ticksToPlace = delayBetweenPlacements;
         }
     }
 
-    private int findCrystalSlot() {
-        for (int i = 0; i < 9; i++) {
-            ItemStack stack = mc.player.inventory.getStackInSlot(i);
-            if (stack.getItem() == Items.END_CRYSTAL) {
-                return i;
+    private EntityEnderCrystal findCrystal() {
+        double maxRangeSq = Math.pow(placeRange, 2);
+        EntityEnderCrystal targetCrystal = null;
+        double closestDistSq = Double.MAX_VALUE;
+
+        for (Entity entity : mc.world.loadedEntityList) {
+            if (entity instanceof EntityEnderCrystal) {
+                double distSq = mc.player.getDistanceSq(entity);
+                boolean isVisible = onlyVisible ? mc.player.canEntityBeSeen(entity) : true;
+
+                if (distSq <= maxRangeSq && distSq < closestDistSq && isVisible) {
+                    closestDistSq = distSq;
+                    targetCrystal = (EntityEnderCrystal) entity;
+                }
             }
         }
-        return -1;
+        return targetCrystal;
+    }
+
+    private void placeCrystal(EntityEnderCrystal crystal) {
+        BlockPos blockPos = crystal.getPosition();
+        EnumFacing enumFacing = EnumFacing.UP;
+
+        mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(blockPos, enumFacing, EnumHand.MAIN_HAND, 0, 0, 0));
+        mc.player.swingArm(EnumHand.MAIN_HAND);
+        ticksSinceLastPlace = 0;
+    }
+
+    private void predictPlayerMovement(EntityEnderCrystal crystal) {
+        if (mc.player != null && mc.player.getDistanceSq(crystal) < 25) {
+            BlockPos predictedPos = new BlockPos(
+                mc.player.posX + mc.player.motionX * 2,
+                mc.player.posY,
+                mc.player.posZ + mc.player.motionZ * 2
+            );
+            placeCrystal(crystal, predictedPos);
+        } else {
+            placeCrystal(crystal);
+        }
+    }
+
+    private void placeCrystal(EntityEnderCrystal crystal, BlockPos position) {
+        mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(position, EnumFacing.UP, EnumHand.MAIN_HAND, 0, 0, 0));
+        mc.player.swingArm(EnumHand.MAIN_HAND);
+        ticksSinceLastPlace = 0;
+    }
+
+    private int calculateSmartDelay() {
+        if (ticksSinceLastPlace < delayBetweenPlacements)
+            return delayBetweenPlacements - ticksSinceLastPlace;
+        else
+            return 0;
     }
 
     @Override
@@ -65,12 +133,24 @@ public class AutoCrystal extends Module {
     @Override
     public void onDisable() {
         MinecraftForge.EVENT_BUS.unregister(this);
-        crystalSlot = -1;
     }
 
     @Override
-    public void toggle() {
-        super.toggle();
-        shouldSwitch = true;
+    public void onSettingChange(Setting setting) {
+        super.onSettingChange(setting);
+
+        if (setting.getName().equals("Place Range")) {
+            placeRange = setting.getIntegerValue();
+        } else if (setting.getName().equals("Delay Between Placements")) {
+            delayBetweenPlacements = setting.getIntegerValue();
+        } else if (setting.getName().equals("Only Visible")) {
+            onlyVisible = setting.getBooleanValue();
+        } else if (setting.getName().equals("Smart Delay")) {
+            smartDelay = setting.getBooleanValue();
+        } else if (setting.getName().equals("Switch Enabled")) {
+            switchEnabled = setting.getBooleanValue();
+        } else if (setting.getName().equals("Killer")) {
+            killerEnabled = setting.getBooleanValue();
+        }
     }
 }
